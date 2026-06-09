52ac2ff2359cf5eb76ee550330c1438bad058ead
Ezekiel <iniobongezek4@gmail.com>
2026-06-09 10:59:34 +0100

    Revert "switch from  OMDB_API_KEY to TMDB_API_KEY"
    This reverts commit 8ec69577f59b77a2d12006450fe38555bbf45e60.

--- api/search.js
+++ api/search.js
@@ -1,16 +1,8 @@
 export default async function handler(req, res) {
     const searchTerm = req.query.q;
-    const apiKey = process.env.TMDB_API_KEY;
+    const apiKey = process.env.OMDB_API_KEY;
 
-    let url;
-
-    if (searchTerm) {
-        url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchTerm}`;
-    } else {
-        url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`;
-    }
-
-    const response = await fetch(url);
+    const response = await fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=${apiKey}`);
     const data = await response.json();
 
     res.status(200).json(data);
