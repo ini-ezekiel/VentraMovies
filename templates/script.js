35cda6b5d5da2fbfbbf517045f4d795775347029
Ezekiel <iniobongezek4@gmail.com>
2026-06-09 11:09:22 +0100

    back-to-old-version

--- templates/script.js
+++ templates/script.js
@@ -1,39 +1,39 @@
 const movieContainer = document.getElementById('movie-container');
 
 function searchMovies() {
-    const searchTerm = document.getElementById('query').value;
-    if (searchTerm === '') {
-        movieContainer.innerHTML = '<p>Please type something first!</p>';
-        return;
-    }
-    fetchMovies(searchTerm);
+  const searchTerm = document.getElementById('query').value;
+
+  if (searchTerm === '') {
+    movieContainer.innerHTML = '<p>Please type something first!</p>';
+    return;
+  }
+
+  fetchMovies(searchTerm);
 }
 
 const fetchMovies = async (searchTerm) => {
-    try {
-        movieContainer.innerHTML = '<p>Loading...</p>';
+  try {
+    movieContainer.innerHTML = '<p>Loading...</p>';
 
-        let url = searchTerm ? `/api/search?q=${searchTerm}` : `/api/search`;
+    const response = await fetch(`/api/search?q=${searchTerm}`);
+    const data = await response.json();
 
-        const response = await fetch(url);
-        const data = await response.json();
+    movieContainer.innerHTML = '';
 
-        movieContainer.innerHTML = '';
-
-        if (data.results && data.results.length > 0) {
-            data.results.forEach(movie => {
-                const movieDiv = document.createElement('div');
-                movieDiv.classList.add('movie');
-                movieDiv.innerHTML = `<img src="https://image.tmdb.org/t/p/w200${movie.poster_path}" alt="${movie.title}"> <div class="info"> <h2>${movie.title}</h2> <p>${movie.release_date?.split('-')[0]}</p> </div>`;
-                movieContainer.appendChild(movieDiv);
-            });
-        } else {
-            movieContainer.innerHTML = `<p>No movies found.</p>`;
-        }
-    } catch (error) {
-        console.error('Error fetching movie data:', error);
-        movieContainer.innerHTML = `<p>There was an error fetching the movie data.</p>`;
+    if (data.Response === "True") {
+      data.Search.forEach(movie => {
+        const movieDiv = document.createElement('div');
+        movieDiv.classList.add('movie');
+        movieDiv.innerHTML = `<img src="${movie.Poster}" alt="${movie.Title}"> <div class="info"> <h2>${movie.Title}</h2> <p>${movie.Year}</p> </div>`;
+        movieContainer.appendChild(movieDiv);
+      });
+    } else {
+      movieContainer.innerHTML = `<p>No movies found for "${searchTerm}"</p>`;
     }
+  } catch (error) {
+    console.error('Error fetching movie data:', error);
+    movieContainer.innerHTML = `<p>There was an error fetching the movie data.</p>`;
+  }
 };
 
-fetchMovies();
+fetchMovies('action')
